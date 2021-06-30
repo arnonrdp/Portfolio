21ff527b7666964f141e569d4d156b2498ef3689
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-06-30 07:15:38 -0300

    Favicon Adjustments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,13 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="shortcut icon mask-icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
 
           <meta name="description" content="Portfolio, side projects, the blog of the Arnon Rodrigues de Paula and among other things" />
           <meta property="og:locale" content="en" />
