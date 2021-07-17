f5ce6d69128e0f1171cc0a5bc4f9ad4e56742d40
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-17 12:41:41 -0300

    Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,12 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="msapplication-TileColor" content="#da532c" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#22c1c3" />
+          <meta name="msapplication-TileColor" content="#22c1c3" />
           <meta name="theme-color" content="#ffffff" />
 
           <meta name="description" content="Portfolio, side projects, the blog of the Arnon Rodrigues de Paula and among other things" />
