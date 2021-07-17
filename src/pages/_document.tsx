d2b5b8990a994d6c6f4915743009123637302a7a
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-17 13:12:58 -0300

    Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,8 +10,6 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#22c1c3" />
-          <meta name="msapplication-TileColor" content="#22c1c3" />
           <meta name="theme-color" content="#ffffff" />
 
           <meta name="description" content="Portfolio, side projects, the blog of the Arnon Rodrigues de Paula and among other things" />
