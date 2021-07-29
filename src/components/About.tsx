f31cde4d8347660b6fa076c78af153461cf8448f
Arnon Rodrigues de Paula <arnon@MacBook-Air-de-Arnon.local>
2021-07-29 19:51:06 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export function About() {
   return (
     <section id={styles.about}>
       <p className={styles.greetings}>👋🏼 Hi, my name is</p>
-      <h1>Arnon Rodrigues</h1>
+      <h1>Tales Melo</h1>
       <h2>I build things for<br />the web and mobile.</h2>
       <p className={styles.description}>I am brazilian 🇧🇷 and a front-end developer who loves to apply technology to solve problems. I have experience in websites with UX/UI design, SEO best practice and agile methodology with SCRUM. I am communicative, dedicated and always interested in learning new technologies and improving my skills.</p>
     </section>
