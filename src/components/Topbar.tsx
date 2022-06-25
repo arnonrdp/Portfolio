088e8d81249cf9e3ae9c9923793a7d6418f50aa4
Arnon <arnonrdp@gmail.com>
2022-06-25 20:46:50 -0300

    Tooltip removed

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,6 @@
-import { Button } from "./Button";
-import { Tooltip } from "./Tooltip";
-import styles from "../styles/components/TopBar.module.css";
 import Image from "next/image";
+import styles from "../styles/components/TopBar.module.css";
+import { Button } from "./Button";
 
 export function Topbar() {
   return (
@@ -9,18 +8,14 @@ export function Topbar() {
       <Image src="/logo.png" alt="Arnon Rodrigues" width={120} height={120} className={styles.avatar} />
 
       <nav className={styles.navBar}>
-        <Tooltip label="E-mail">
+        <div>
           <Button link={"mailto:me@arnon.dev"} image={"/icons/email.svg"} />
-        </Tooltip>
-        <Tooltip label="WhatsApp">
           <Button link={"https://wa.me/5521971111312"} image={"/icons/whatsapp.svg"} />
-        </Tooltip>
-        <Tooltip label="LinkedIn">
+        </div>
+        <div>
           <Button link={"https://linkedin.com/in/arnonrdp"} image={"/icons/linkedin.svg"} />
-        </Tooltip>
-        <Tooltip label="GitHub">
           <Button link={"https://github.com/arnonrdp"} image={"/icons/github.svg"} />
-        </Tooltip>
+        </div>
       </nav>
     </header>
   );
