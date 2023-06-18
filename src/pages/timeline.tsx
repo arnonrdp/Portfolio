44df61802252159c59bcfc80c7f5fab797b1c737
Arnon <arnonrdp@gmail.com>
2023-06-18 18:33:21 -0300

    Adicionado Timeline

---
+++ src/pages/timeline.tsx
@@ -0,0 +1,25 @@
+import timeline from '../data/timeline.json'
+import styles from '../styles/components/Timeline.module.css'
+
+export default function Timeline() {
+  return (
+    <>
+      <ul className={styles.timeline}>
+        {timeline.map((item, index) => (
+          <li key={index}>
+            <div className={item.education ? styles.direction_l : styles.direction_r}>
+              <div className={styles.flag_wrapper}>
+                <span className={styles.flag}>{item.title}</span>
+                <span className={styles.time_wrapper}>
+                  <span className={styles.time}>{item.time}</span>
+                </span>
+              </div>
+              <div className={styles.desc}>{item.description}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <p>past</p>
+    </>
+  )
+}
